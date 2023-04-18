37508aa8643321593f50c99db5bdb7f511c2995d
Lucas Henrique Chaves <103153987+lucasxae@users.noreply.github.com>
2023-04-18 14:15:13 -0300

    Add files via upload

---
+++ Fundamentos Csharpe/Cronometro/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo");
+            Console.WriteLine("M = Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1,1)); // contar qnt de caracteres e pegar o ultimo caracter
+            int time = int.Parse(data.Substring(0,data.Length-1)); // pegar todos os caracteres menos o ultimo que seria o "s"
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+
+
+        }
+
+        static void PreStart(int time) 
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+        }
+        static void Start( int time)
+        {
+           
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch Finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
