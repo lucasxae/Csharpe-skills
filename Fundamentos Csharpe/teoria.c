0a5fe5998de0a2acfaaf66426668726f6f73bf9a
Lucas Henrique Chaves <103153987+lucasxae@users.noreply.github.com>
2023-04-18 14:01:59 -0300

    Update teoria.c

--- Fundamentos Csharpe/teoria.c
+++ Fundamentos Csharpe/teoria.c
@@ -1,1 +1,40 @@
+// Strings em C#
 
+StartsWith = Comeca com ...
+
+EndsWith = Termina com ...
+
+Guids = indentificador unico (numero gigante que nao se repete)
+
+Equals = Comparar 
+
+Contains = variavel contem ...
+
+CompareTo = comparar variaveis e retornar um inteiro
+
+
+Indices = usados em litas, string e uma lista de caracteres q formam uma string
+
+IndexOf = conta a posicao do caracter que vc indica
+
+LastIndexOf = ultimo indice que ele encontrar na string 
+
+ToLower = converter o texto pra Minusculo
+
+ToUpper = converter o texto pra Maiusculo 
+
+Insert = inserir um caracter no texto (indice, "O texto que vc quer inserir")
+
+Remove = remover um caracter do texto (indice pra comecar, quantidade de indices que vc quer remover)
+
+Length = quantidade de caracter q uma string tem
+
+Replace = Trocar caracteres na strings
+
+Split = dividir caracteres da strings, vai retornar uma lista da string 
+
+Substring = pega um caracter especifico, determinando a posicao e a qnt de caracter depois dessa posicao
+
+Trim = limpar os espacos do comeco e final 
+
+StringBuilder = dentro do (System.text), var.Append => quebrar linha e entender como uma linha so
